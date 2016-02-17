df7e1ac82c701fd6d8ece27fb60e604899e3608d
miraesn08 <miraesn08@gmail.com>
2016-02-17 17:52:32 +0900

    add CardViewActivity - read state

--- app/src/main/java/kr/co/bit/osf/flashcard/CardViewActivity.java
+++ app/src/main/java/kr/co/bit/osf/flashcard/CardViewActivity.java
@@ -1,13 +1,26 @@
 package kr.co.bit.osf.flashcard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import kr.co.bit.osf.flashcard.db.FlashCardDB;
+import kr.co.bit.osf.flashcard.db.StateDTO;
 
 public class CardViewActivity extends AppCompatActivity {
+    final String TAG = "FlashCardCardViewTag";
+
+    FlashCardDB db = null;
+    StateDTO cardState = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
+
+        // read state from db
+        db = new FlashCardDB(this);
+        cardState = db.getState();
+        Log.i(TAG, "read card state:" + cardState);
     }
 }
