efa1dd2451e297cbc0013c898d31816dcbee9366
miraesn08 <miraesn08@gmail.com>
2016-02-17 17:26:01 +0900

    add MainActivity - db initialize

--- app/src/main/java/kr/co/bit/osf/flashcard/MainActivity.java
+++ app/src/main/java/kr/co/bit/osf/flashcard/MainActivity.java
@@ -1,13 +1,36 @@
 package kr.co.bit.osf.flashcard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import kr.co.bit.osf.flashcard.db.FlashCardDB;
+import kr.co.bit.osf.flashcard.db.StateDAO;
+import kr.co.bit.osf.flashcard.db.StateDTO;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "FlashCardMainTag";
+
+    FlashCardDB db = null;
+    StateDAO stateDao = null;
+    StateDTO state = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // db initialize
+        db = new FlashCardDB(this);
+        stateDao = db;
+        state = stateDao.getState();
+        if (state == null) {
+            Log.i(TAG, "db initialize");
+            db.initialize();
+            state = stateDao.getState();
+            Log.i(TAG, "state:" + state);
+        } else {
+            Log.i(TAG, "db already initialized");
+        }
     }
 }
