44243cd03a48c5f5d47bd2d494770d440b2a198a
KimDong-yeong <noahkim.j@gmail.com>
2016-02-17 16:23:40 +0900

    add Empty Activity

---
+++ app/src/main/java/kr/co/bit/osf/flashcard/CardViewActivity.java
@@ -0,0 +1,13 @@
+package kr.co.bit.osf.flashcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
+    }
+}
