084fbb0c6fa13d03d2e0dfff437ad37ad7881aba
KimDong-yeong <noahkim.j@gmail.com>
2016-02-18 14:27:51 +0900

    add  initial state template setting for test

---
+++ app/src/main/java/kr/co/bit/osf/flashcard/test/SetupInitialStateTemplate.java
@@ -0,0 +1,16 @@
+package kr.co.bit.osf.flashcard.test;
+
+import kr.co.bit.osf.flashcard.db.StateDAO;
+
+
+//테스트를 위한 state 설정 template
+//Test환경에 맞는 초기 Activity화면으로 이동시킨다.
+// class SetupInitialState를 만드세요.
+
+
+public class SetupInitialStateTemplate {
+    public static void setupState(StateDAO dao) {
+        // state값을 설정, DB에 저장
+        // dao.updateState(new StateDTO(1,1,1)); //예
+    }
+}
