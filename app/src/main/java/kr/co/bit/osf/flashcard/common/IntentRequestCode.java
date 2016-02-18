75cd7d257fa81d87a0c1cd4f8950921b89e80e09
KimDong-yeong <noahkim.j@gmail.com>
2016-02-18 15:17:29 +0900

    add Intent request code

---
+++ app/src/main/java/kr/co/bit/osf/flashcard/common/IntentRequestCode.java
@@ -0,0 +1,15 @@
+package kr.co.bit.osf.flashcard.common;
+
+public interface IntentRequestCode {
+    int CARD_ADD = 1001;
+    int CARD_DELETE = 1002;
+    int CARD_EDIT = 1003;
+    int CARD_DELETE_LIST = 1004;
+
+    int BOX_ADD = 1101;
+    int BOX_DELETE = 1102;
+    int BOX_EDIT = 1103;
+
+    int CAPTURE_IMAGE = 1201;
+    int SELECT_PICTURE = 1202;
+}
