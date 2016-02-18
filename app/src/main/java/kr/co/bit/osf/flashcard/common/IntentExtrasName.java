29b1e2c883c456b6351c05d796c026e65b13ba1d
miraesn08 <miraesn08@gmail.com>
2016-02-18 20:27:35 +0900

    add intent extras name

---
+++ app/src/main/java/kr/co/bit/osf/flashcard/common/IntentExtrasName.java
@@ -0,0 +1,7 @@
+package kr.co.bit.osf.flashcard.common;
+
+public interface IntentExtrasName {
+    String REQUEST_CODE = "requestCode";
+    String SEND_DATA = "sendData";
+    String RETURN_DATA = "returnData";
+}
