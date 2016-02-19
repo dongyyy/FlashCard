feed135b2e7f0a14ae531252f53273d46576150b
KimDong-yeong <noahkim.j@gmail.com>
2016-02-19 17:46:32 +0900

    add CardEditActivity - image, text show

--- app/src/main/java/kr/co/bit/osf/flashcard/CardEditActivity.java
+++ app/src/main/java/kr/co/bit/osf/flashcard/CardEditActivity.java
@@ -3,16 +3,23 @@ package kr.co.bit.osf.flashcard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import kr.co.bit.osf.flashcard.common.IntentExtrasName;
 import kr.co.bit.osf.flashcard.common.IntentRequestCode;
 import kr.co.bit.osf.flashcard.db.CardDTO;
+import kr.co.bit.osf.flashcard.db.FlashCardDB;
 import kr.co.bit.osf.flashcard.debug.Dlog;
 
 public class CardEditActivity extends AppCompatActivity {
     private int intentRequestCode = 0;
     private int intentResultCode = RESULT_CANCELED;
     private CardDTO card = null;
+    private TextView textView = null;
+    private ImageView imageView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +44,23 @@ public class CardEditActivity extends AppCompatActivity {
 
         // todo: process requested task
         Dlog.i("getExtras:sendData:" + card);
-        card.setName("is updated");
         intentResultCode = RESULT_OK;
+        textView = (TextView)findViewById(R.id.cardEditTextView);
+        imageView = (ImageView)findViewById(R.id.cardEditImageView);
+
+        //show card
+        if( intentRequestCode == IntentRequestCode.CARD_EDIT){
+            String imagePath = card.getImagePath();
+            if (card.getType() == FlashCardDB.CardEntry.TYPE_USER) {
+                // load image from sd card(glide)
+                Glide.with(getApplicationContext()).load(imagePath).into(imageView);
+            } else {
+                // card demo data(glide)
+                Glide.with(getApplicationContext()).fromResource()
+                        .load(Integer.parseInt(imagePath)).into(imageView);
+            }
+        }
+        textView.setText(card.getName());
     }
 
     @Override
