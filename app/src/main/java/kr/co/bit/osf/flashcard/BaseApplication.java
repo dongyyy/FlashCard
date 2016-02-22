4996ed7e3717eeaa1eef4fc6cb8222afdb3d3161
miraesn08 <miraesn08@gmail.com>
2016-02-22 09:25:27 +0900

    add BaseApplication - db initialize

--- app/src/main/java/kr/co/bit/osf/flashcard/BaseApplication.java
+++ app/src/main/java/kr/co/bit/osf/flashcard/BaseApplication.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 
+import kr.co.bit.osf.flashcard.db.FlashCardDB;
+import kr.co.bit.osf.flashcard.db.StateDTO;
+import kr.co.bit.osf.flashcard.debug.Dlog;
+
 // http://gun0912.tistory.com/12
 public class BaseApplication extends Application {
     public static boolean DEBUG = false;
@@ -13,6 +17,18 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         this.DEBUG = isDebuggable(this);
+
+        // db initialize
+        FlashCardDB db = new FlashCardDB(this);
+        StateDTO state = db.getState();
+        if (state == null) {
+            Dlog.i("db initialize");
+            db.initialize();
+            state = db.getState();
+            Dlog.i("state:" + state);
+        } else {
+            Dlog.i("db already initialized");
+        }
     }
 
     /**
