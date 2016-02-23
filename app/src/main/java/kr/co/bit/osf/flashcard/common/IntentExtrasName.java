fd1e0cb8eec47546850f042b98c7d93da69a8064
miraesn08 <miraesn08@gmail.com>
2016-02-23 15:08:10 +0900

    add IntentExtrasName - return code

--- app/src/main/java/kr/co/bit/osf/flashcard/common/IntentExtrasName.java
+++ app/src/main/java/kr/co/bit/osf/flashcard/common/IntentExtrasName.java
@@ -4,4 +4,5 @@ public interface IntentExtrasName {
     String REQUEST_CODE = "requestCode";
     String SEND_DATA = "sendData";
     String RETURN_DATA = "returnData";
+    String RETURN_CODE = "returnCode";
 }
